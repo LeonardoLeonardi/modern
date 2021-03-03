e6ec1dcbd7ddbfd91574cdf178ae1c369452491c
LeonardoLeonardi <leonardoleonardi@keix.com>
2021-03-03 12:45:01 +0100

    orrore

--- src/App.tsx
+++ src/App.tsx
@@ -3,45 +3,45 @@ import './App.css';
 import faker from 'faker';
 import arrayMove from 'array-move';
 import People from './People';
-import { update, set } from 'lodash';
+import { update, set, clone } from 'lodash';
 
-interface Item {}
+interface Item {
+  name: string;
+  favorite: boolean;
+}
 
 interface Action {
   type: string;
   index: number;
 }
 
-function reducer(state: any, action: Action) {
+function reducer(state: Item[], action: Action) {
   switch (action.type) {
     case 'Add':
       const newPerson = { name: faker.name.findName(), favorite: false };
-      state.push(newPerson);
-      return;
-      break;
+      return [...state, newPerson];
     case 'Remove':
-      state.filter((list: any, index: number) => index !== action.index);
-      break;
+      return state.filter((list: any, index: number) => index !== action.index);
     case 'MoveUp':
-      state = arrayMove(state, action.index - 1, action.index);
-      break;
+      return arrayMove(state, action.index - 1, action.index);
     case 'MoveDown':
-      state = arrayMove(state, action.index + 1, action.index);
-      break;
+      return arrayMove(state, action.index + 1, action.index);
     case 'Favorite':
-      update(state, `${action.index}.favorite`, (prev: boolean) => !prev);
-      break;
+      return update(
+        [...state],
+        `${action.index}.favorite`,
+        (prev: boolean) => !prev,
+      );
     default:
       throw new Error();
   }
   return state;
 }
+const peopleArray: Item[] = new Array(10)
+  .fill({})
+  .map((list) => ({ name: faker.name.findName(), favorite: false }));
 
 function App(props: Item) {
-  const peopleArray = new Array(10)
-    .fill({})
-    .map((list) => ({ name: faker.name.findName(), favorite: false }));
-
   const [state, dispatch] = useReducer(reducer, peopleArray);
 
   function ButtonAdd() {
@@ -58,7 +58,7 @@ function App(props: Item) {
   return (
     <div>
       <People
-        people={peopleArray}
+        people={state}
         onClickRemove={(index) => dispatch({ type: 'Remove', index })}
         onClickUp={(index) => dispatch({ type: 'MoveUp', index })}
         onClickDown={(index) => dispatch({ type: 'MoveDown', index })}
